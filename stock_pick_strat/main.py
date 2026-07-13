813120a3cb5de1d74e9479b4147fbe2c9cc7ac69
alexandredelarrard <a.larrard@gmail.com>
2026-07-12 23:37:15 -0400

    kind of working now, but suspocious

--- stock_pick_strat/main.py
+++ stock_pick_strat/main.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     # self = StepBuildCube(context=context, config=config)
     # self.run()
 
-    self = StepModelling(context=context, config=config)
-    self.run()
+    # self = StepModelling(context=context, config=config)
+    # self.run()
 
     self = StepBacktest(context=context, config=config)
     self.run() 

5659b3c8d546b0a7c64e52d14e0bfd2c558fc657
alexandredelarrard <a.larrard@gmail.com>
2026-07-19 10:56:36 -0400

    local

--- stock_pick_strat/src/data_extract/step_extract_structure.py
+++ stock_pick_strat/src/data_extract/step_extract_structure.py
@@ -12,7 +12,6 @@ from omegaconf import DictConfig
 from src.context import Context
 from src.utils.step import Step
 from src.data_extract.utils.structure.fetch_employees_edgar import fetch_employees_edgar
-from src.data_extract.utils.structure.fetch_management_edgar import fetch_management_edgar
 from src.data_extract.utils.structure.fetch_def14a_llm import fetch_def14a_llm
 from src.data_extract.utils.structure.fetch_sec_filings import fetch_sec_filings
 
@@ -23,8 +22,7 @@ class StepExtractStructure(Step):
         super().__init__(context=context, config=config)
 
     def run(self, tickers: list[str]) -> None:
-        # fetch_employees_edgar(self._context, tickers=tickers)
-        # fetch_management_edgar(self._context, tickers=tickers)
+        fetch_employees_edgar(self._context, tickers=tickers)
         fetch_def14a_llm(self._context, tickers=tickers,
                         model=self._config.data_extract.llm_model)
-        # fetch_sec_filings(self._context)
+        fetch_sec_filings(self._context)
